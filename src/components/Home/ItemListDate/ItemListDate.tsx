704583b8deec00d63545257458822dc0c83bb4be
Damian <damianmorganti14@gmail.com>
2025-03-11 16:49:44 -0400

    added itemlistdate

---
+++ src/components/Home/ItemListDate/ItemListDate.tsx
@@ -0,0 +1,57 @@
+import React, {useContext} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import Icon from "../../../../mk/components/ui/Icon/Icon";
+import { IconArrowLeft, IconArrowRight } from "../../../icons/IconLibrary";
+import { cssVar, FONTS } from "../../../../mk/styles/themes";
+import { getDateTimeStrMes } from "../../../../mk/utils/dates";
+
+
+interface PropsType {
+  date?: string | null;
+  data1?: string | null;
+  data2?: string | null;
+}
+
+const ItemListDate = ({data1 = null, data2 = null, date = ""}: PropsType) => {
+  return (
+    <View
+      style={styles.container}>
+      <View style={{width: "86%"}}>
+        {data1 && (
+          <View style={styles.center}>
+            <Icon
+              size={20}
+              name={IconArrowRight}
+              color={cssVar.cSuccess}
+            />
+            <Text style={styles.fecha}>{getDateTimeStrMes(data1)}</Text>
+          </View>
+        )}
+        {data2 && (
+          <View style={styles.center}>
+            <Icon size={20} name={IconArrowLeft} color={cssVar.cError} />
+            <Text style={styles.fecha}>{getDateTimeStrMes(data2)}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    width: "100%",
+    alignItems: "flex-end",
+  },
+  center:{
+    flexDirection: "row", 
+    alignItems: "center"
+  },
+  fecha: {
+    fontSize: 12,
+    fontFamily: FONTS.regular,
+    color: cssVar.cWhiteV2,
+  },
+
+});
+export default ItemListDate;
