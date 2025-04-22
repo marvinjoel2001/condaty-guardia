c739444ea628da5f026bcc08ebec7362c83ec9b7
mario Apple <gestion.codigo.condaty@gmail.com>
2025-04-22 11:03:29 -0400

    refactor(App.tsx): remove unused imports and commented code
    Clean up the App.tsx file by removing unused React imports, commented-out code, and redundant comments to improve code readability and maintainability.

--- App.tsx
+++ App.tsx
@@ -1,30 +1,16 @@
-import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {useEffect} from 'react';
 import AxiosProvider from './mk/contexts/AxiosContext';
 import axiosInterceptors from './mk/interceptors/axiosInterceptors';
 import AuthProvider from './mk/contexts/AuthContext';
-import PusherContextProvider from './mk/contexts/PusherContext';
 import OneSignalContextProvider from './mk/contexts/OneSignalContext';
 import InitProject from './src/config/InitProject';
 import MyDrawer from './src/navigators/Drawer/Drawer';
-// import SplashScreen from 'react-native-splash-screen';
-import {StatusBar, Text} from 'react-native';
+import {StatusBar} from 'react-native';
 import {cssVar} from './mk/styles/themes';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import SplashScreen from 'react-native-splash-screen';
 import ActiveNotificationDB from './mk/hooks/ActiveNotificationDB';
 
 function App() {
-  // Text.defaultProps = Text.defaultProps || {};
-  // Text.defaultProps.allowFontScaling = false;
-
-  // useEffect(() => {
-  //   // setTimeout(() => {
-  //   SplashScreen.hide();
-  //   // }, 2000);
-  // }, []);
-
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <AxiosProvider interceptors={axiosInterceptors}>
@@ -33,10 +19,8 @@ function App() {
           <AuthProvider>
             <ActiveNotificationDB />
             <OneSignalContextProvider>
-              {/* <PusherContextProvider> */}
               <InitProject />
               <MyDrawer />
-              {/* </PusherContextProvider> */}
             </OneSignalContextProvider>
           </AuthProvider>
         </NavigationContainer>
