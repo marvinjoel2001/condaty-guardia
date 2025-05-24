b028d44db79b964c4198b948dc12add8a93e5ab9
cristian <crisblanco.fos@gmail.com>
2025-05-24 12:10:09 -0400

    style(Login): swap and adjust login screen text and styles
    Update the login screen by swapping the title and subtitle text for better clarity and adjust the font sizes, colors, and margins to improve visual consistency. The changes enhance the user experience by making the interface more intuitive and aesthetically pleasing.

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -161,8 +161,8 @@ const Login = () => {
             resizeMode="cover"
           />
           <View style={theme.container}>
-            <Text style={theme.titleLogin}>Guardia</Text>
-            <Text style={theme.subtitleLogin}>Bienvenido</Text>
+            <Text style={theme.titleLogin}>¡Te damos la bienvenida!</Text>
+            <Text style={theme.subtitleLogin}>Guardia</Text>
             <Input
               label="Cédula de identidad"
               name="email"
@@ -309,15 +309,15 @@ const theme: ThemeType = {
   titleLogin: {
     color: cssVar.cWhite,
     fontFamily: FONTS.bold,
-    fontSize: 32,
-    paddingBottom: 8,
+    fontSize: 24,
     textAlign: 'center',
   },
   subtitleLogin: {
-    color: cssVar.cWhite,
+    color: cssVar.cWhiteV1,
     textAlign: 'center',
+    fontFamily: FONTS.regular,
     fontSize: 14,
-    paddingBottom: 8,
+    marginBottom: 24,
   },
 
   container: {
