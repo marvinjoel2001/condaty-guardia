23eb5bf37023d043642f36637e3b65d09c6888ee
Esteban Hurtado Blumberg <damianpinedo.fos@gmail.com>
2025-10-11 14:14:45 -0400

    Optimizando el input

--- mk/components/forms/Input/Input.tsx
+++ mk/components/forms/Input/Input.tsx
@@ -1,64 +1,78 @@
-import {useState} from 'react';
+import React, {useState, useMemo, useCallback} from 'react';
 import {Platform, StyleSheetProperties, TextInput} from 'react-native';
 import ControlLabel, {PropsTypeInputBase} from '../ControlLabel/ControlLabel';
 import {cssVar, FONTS, ThemeType} from '../../../styles/themes';
-import React from 'react';
+
 interface PropsType extends PropsTypeInputBase {
   passwordMaxLength?: number;
   ciMaxLength?: number;
   iconLeft?: any;
   iconRight?: any;
 }
+
 const Input = (props: PropsType) => {
   const [isFocused, setIsFocused] = useState(false);
 
-  const handleTextChange = (text: string) => {
+  const handleTextChange = useCallback((text: string) => {
     if (props.onChange) {
       props.onChange(text);
     }
-  };
+  }, [props.onChange]);
 
-  const styleInput = {
+  const styleInput = useMemo(() => ({
     ...{...theme.default, paddingLeft: props.iconLeft ? 30 : cssVar.spS},
     ...{paddingRight: props.iconRight ? 30 : cssVar.spM},
     ...(isFocused ? theme.focusInput : {}),
     ...(props.error && props.error[props.name] ? {...theme.errorInput} : {}),
     ...props.style,
     ...(props.disabled ? theme.disabled : {}),
-  };
+  }), [isFocused, props.error, props.name, props.style, props.disabled, props.iconLeft, props.iconRight]);
 
-  const _onBlur = (e: any) => {
+  const _onBlur = useCallback((e: any) => {
     setIsFocused(false);
     if (props.onBlur) {
       props.onBlur(e);
     }
-  };
+  }, [props.onBlur]);
+
+  const _onFocus = useCallback((e: any) => {
+    setIsFocused(true);
+    if (props.onFocus) {
+      props.onFocus(e);
+    }
+  }, [props.onFocus]);
+
+  const textColor = useMemo(() => 
+    props.disabled ? theme.disabledInput?.color : theme.default?.color,
+    [props.disabled]
+  );
+
+  const keyboardType = useMemo(() => {
+    if (props.keyboardType) {
+      return props.keyboardType;
+    }
+    if (props.type === 'date') {
+      return 'numeric';
+    }
+    if (props.type === 'email-address') {
+      return 'email-address';
+    }
+    return 'default';
+  }, [props.keyboardType, props.type]);
 
   return (
     <ControlLabel {...props} isFocus={isFocused}>
       <TextInput
         testID={props.name}
         id={props.name}
-        onFocus={e => {
-          setIsFocused(true), props.onFocus && props.onFocus(e);
-        }}
+        onFocus={_onFocus}
         onBlur={_onBlur}
         onSubmitEditing={props.onSearch}
         style={{
-          color: props.disabled
-            ? theme.disabledInput?.color
-            : theme.default?.color,
+          color: textColor,
           ...styleInput,
         }}
-        keyboardType={
-          props.keyboardType
-            ? props.keyboardType
-            : props.type === 'date'
-            ? 'numeric'
-            : props.type === 'email-address'
-            ? 'email-address'
-            : 'default'
-        }
+        keyboardType={keyboardType}
         maxLength={props.maxLength}
         onChangeText={handleTextChange}
         value={props.value}
