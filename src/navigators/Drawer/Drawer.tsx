91709b2caee051130bb187aa044a12f6b2ffa271
Esteban Hurtado Blumberg <damianpinedo.fos@gmail.com>
2025-10-14 11:04:57 -0400

    actualiacion de drawer.tsx

--- src/navigators/Drawer/Drawer.tsx
+++ src/navigators/Drawer/Drawer.tsx
@@ -1,40 +1,43 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Navigate from '../Navigate';
 import MainMenu from './MainMenu';
-import {SafeAreaView} from 'react-native';
 import {cssVar} from '../../../mk/styles/themes';
 import React from 'react';
+import {SafeAreaView as SafeAreaViewAndroid} from 'react-native-safe-area-context';
+import {SafeAreaView as SafeAreaViewIos} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 const MyDrawer = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: cssVar.cBlack}}>
-      <Drawer.Navigator
-        initialRouteName="navigate"
-        screenOptions={({navigation}) => ({
-          // headerLeft: () => <Hamburguer onPress={navigation.toggleDrawer} />,
-          drawerStyle: {backgroundColor: cssVar.cBlack},
-          // headerTransparent: true,
-          swipeEnabled: true,
-          // headerStyle: theme.layout?.header,
-          // headerBackgroundContainerStyle: theme.layout?.header,
-          headerShown: false,
-        })}
-        drawerContent={props => <MainMenu {...props} />}>
-        <Drawer.Screen
-          name="navigate"
-          // options={
-          //   {
-          //       headerTitleStyle: {
-          //         color: pallete[theme.currentTheme]?.light?.color,
-          //       },
-          //   }
-          // }
-          component={Navigate}
-        />
-      </Drawer.Navigator>
-    </SafeAreaView>
+    <SafeAreaViewAndroid style={{flex: 1, backgroundColor: cssVar.cBlack}}>
+      <SafeAreaViewIos style={{flex: 1, backgroundColor: cssVar.cBlack}}>
+        <Drawer.Navigator
+          initialRouteName="navigate"
+          screenOptions={({navigation}) => ({
+            // headerLeft: () => <Hamburguer onPress={navigation.toggleDrawer} />,
+            drawerStyle: {backgroundColor: cssVar.cBlack},
+            // headerTransparent: true,
+            swipeEnabled: false,
+            // headerStyle: theme.layout?.header,
+            // headerBackgroundContainerStyle: theme.layout?.header,
+            headerShown: false,
+          })}
+          drawerContent={props => <MainMenu {...props} />}>
+          <Drawer.Screen
+            name="navigate"
+            // options={
+            //   {
+            //       headerTitleStyle: {
+            //         color: pallete[theme.currentTheme]?.light?.color,
+            //       },
+            //   }
+            // }
+            component={Navigate}
+          />
+        </Drawer.Navigator>
+      </SafeAreaViewIos>
+    </SafeAreaViewAndroid>
   );
 };
 export default MyDrawer;
