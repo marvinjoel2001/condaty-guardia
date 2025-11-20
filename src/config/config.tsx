e40b6daf9fe308cf4d227cf981a3a70e9d910df8
cristian <crisblanco.fos@gmail.com>
2025-11-20 14:52:10 -0400

    fix(config): uncomment dev API URL for virtual development
    Enable development API URL for virtual environment testing. Also update department type property access in CiNomModal component.

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -37,7 +37,7 @@ configApp.API_URL = configApp.API_URL_TEST;
 // configApp.API_URL = configApp.API_URL_DEMO;
 
 if (process.env.NODE_ENV == 'development') {
-  //  configApp.API_URL = configApp.API_URL_DEV; // Esto es para desarrollo en virtual
+  configApp.API_URL = configApp.API_URL_DEV; // Esto es para desarrollo en virtual
   // configApp.API_URL = configApp.API_URL_DEMO;
   // configApp.API_URL = configApp.API_URL_TEST;
 }
